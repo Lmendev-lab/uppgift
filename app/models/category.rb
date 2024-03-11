9dcc4526a81eea42756bbeab6b04cb30f6828dbc
Luis Mendoza <lmendoza.mg@gmail.com>
2024-03-11 08:19:54 -0300

    feat(models): Add schema information to Category and Task models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
 class Category < ApplicationRecord
   has_many :tasks
 end
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,24 @@
 class Task < ApplicationRecord
   belongs_to :category
 end
+
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  due_date    :date
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_tasks_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
